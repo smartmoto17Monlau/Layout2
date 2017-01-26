6b9f1c20c6edabb623e84b0d689f9e825d23ccb9
smartmoto17Monlau <smartmoto17.monlau@gmail.com>
2017-01-26 10:01:19 +0100

    update plugins android

---
+++ app/src/main/java/com/example/user/layout/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.user.layout;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    GoogleMap googleMap;
+    MapView mapView;
+    //variables globales
+    private double latitud = 0, longitud =0;
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        findViewById(R.id.textViewScroll).setSelected(true);
+
+        googleMap = mapView.getMap();
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        readGPS();
+        float zoomLevel = 16;
+        googleMap.setMyLocationEnabled(true);
+        LatLng  tuLocation = new LatLng(latitud, longitud);
+        googleMap.addMarker(new MarkerOptions().position(tuLocation).title("Marker en tu posicion"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tuLocation, zoomLevel));
+    }
+
+
+    private void readGPS() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                double altitude = location.getAltitude(); //en metros
+                double latitude = location.getLatitude(); //en grados
+                double longitude = location.getLongitude(); //en grados
+                double speed = location.getSpeed() * 3600 / 1000; //en km/h
+                DecimalFormat df = new DecimalFormat("000.0000");
+                //guardamos las ultimas coordenadas en las que hemos estado
+                latitud = latitude;
+                longitud = longitude;
+                float zoomLevel = 16;
+                LatLng  tuLocation = new LatLng(latitud, longitud);
+                googleMap.addMarker(new MarkerOptions().position(tuLocation).title("Marker en tu posicion"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tuLocation, zoomLevel));
+            }
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+            public void onProviderEnabled(String provider) {
+
+            }
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast toast = Toast.makeText(getApplicationContext(), "no permission", Toast.LENGTH_LONG);
+            toast.show();
+            //si no tiene permisos se los pedimos por pantalla
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+                locationListener);
+    } // fin de readGPS
+
+}
